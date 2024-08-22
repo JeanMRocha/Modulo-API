eae289ba09484a33215a0b738dbdb765bb4002d3
JeanMRocha <app.jmr@gmail.com>
2024-08-22 00:02:01 -0300

    Add initial project setup and configuration files
    - Created .vscode/settings.json for VSCode configuration, including C++ and VC++ formatting settings.
    - Added APSolution.sln for Visual Studio solution file.
    - Introduced Context/AgendaContext.cs for Entity Framework context configuration.
    - Added Entities/Contato.cs to define the Contato entity.
    - Modified Modulo-API.csproj to disable nullable types.
    - Updated Program.cs to include database context configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+//Add server
+builder.Services.AddDbContext<AgendaContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 // Adicionar serviços para controladores
 builder.Services.AddControllers();
